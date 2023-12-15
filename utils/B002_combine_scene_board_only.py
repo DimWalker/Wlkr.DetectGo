b95ccae5472d33c8c707612ec03e2782eda86e53
DimWalker <dimwalker@qq.com>
2023-12-15 18:05:10 +0800

    训练效果不好，暂时改为只检测棋盘，以后再加行列
    Signed-off-by: DimWalker <dimwalker@qq.com>

--- utils/B002_combine_scene_board_only.py
+++ utils/B002_combine_scene_board_only.py
@@ -1,4 +1,3 @@
-# 读取 JPG 图像
 import json
 import os.path
 import random
@@ -6,62 +5,9 @@ from datetime import datetime
 import cv2
 import numpy as np
 
-
-def combine_scene_image(jpg_path, png_path, train_size=640):
-    """
-
-    :param jpg_path:
-    :param png_path:
-    :param train_size: Yolo训练的输出尺寸正方形
-    :return:
-    """
-    jpg_img = cv2.imread(jpg_path)
-    png_img = cv2.imread(png_path, cv2.IMREAD_UNCHANGED)
-    jpg_h, jpg_w, _ = jpg_img.shape
-    png_h, png_w, _ = png_img.shape
-    # 随机偏移
-    offset_x = random.randint(0, jpg_w - png_w)
-    offset_y = random.randint(0, jpg_h - png_h)
-    # 获取 PNG 图像的 Alpha 通道
-    alpha_channel = png_img[:, :, 3]
-    # 将 PNG 图像合成到 JPG 图像中
-    for c in range(0, 3):
-        jpg_img[offset_y:png_h + offset_y, offset_x:png_w + offset_x, c] = \
-            jpg_img[offset_y:png_h + offset_y, offset_x:png_w + offset_x, c] * \
-            (1 - alpha_channel / 255) + png_img[:, :, c] * (alpha_channel / 255)
-
-    if jpg_h > jpg_w:
-        zoom = jpg_h / train_size
-    else:
-        zoom = jpg_w / train_size
-
-    if zoom != 1:
-        offset_x = int(offset_x * zoom)
-        offset_y = int(offset_y * zoom)
-        new_width = int(jpg_w * zoom)
-        new_height = int(jpg_h * zoom)
-        jpg_img = cv2.resize(jpg_img, (new_width, new_height))
-
-    return jpg_img, offset_x, offset_y, zoom
-
-
-def offset_json_obj(json_path, offset_x, offset_y, zoom):
-    with open(json_path, "r", encoding="utf-8") as f:
-        json_obj = json.load(f)
-    for r, row in enumerate(json_obj["matrix"]):
-        for c, cell in enumerate(row):
-            json_obj["matrix"][r][c] = [cell[0] * zoom + offset_x, cell[1] * zoom + offset_y]
-    for p, point in enumerate(json_obj["dst_pts"]):
-        json_obj["dst_pts"][p] = [point[0] * zoom + offset_x, point[1] * zoom + offset_y]
-    for r, row in enumerate(json_obj["regions"]):
-        for c, cell in enumerate(row):
-            for rg, region in enumerate(cell):
-                json_obj["regions"][r][c][rg] = [region[0] * zoom + offset_x, region[1] * zoom + offset_y]
-    return json_obj
-
-
 ################## Coco Dataset
 # 定义类别信息
+from utils.B002_combine_scene import combine_scene_image, offset_json_obj
 
 info = {
     "description": "COCO Dataset",
@@ -75,21 +21,6 @@ licenses_list = []
 categories_list = [
     {
         "id": 1,
-        "name": "black",
-        "supercategory": "piece"
-    },
-    {
-        "id": 2,
-        "name": "white",
-        "supercategory": "piece"
-    },
-    {
-        "id": 3,
-        "name": "empty",
-        "supercategory": "board"
-    },
-    {
-        "id": 4,
         "name": "board",
         "supercategory": "board"
     },
@@ -120,7 +51,7 @@ def find_category_id(name):
     raise Exception("category_id not found.")
 
 
-def coco_image_info(json_obj, jpg_img, dia_path, scene):
+def coco_image_info(json_obj, jpg_img, scene):
     global image_info_id
     global annotation_info_id
     # 获取当前日期和时间
@@ -157,24 +88,6 @@ def coco_image_info(json_obj, jpg_img, dia_path, scene):
         "iscrowd": 0
     }
     ann_list.append(annotation_info)
-    diagram = np.genfromtxt(dia_path, delimiter=' ', dtype=np.int32, encoding="utf-8")
-
-    for r, row in enumerate(json_obj["regions"]):
-        for c, cell in enumerate(row):
-            annotation_info_id += 1
-            seg, area, roi = cale_ppt_from_region(cell)
-            cat_id = find_category_id("black" if diagram[r][c] == 1 else "white" if diagram[r][c] == 2 else "empty")
-            # 定义注释信息
-            annotation_info = {
-                "id": annotation_info_id,
-                "image_id": image_info_id,
-                "category_id": cat_id,  # 替换为实际类别ID
-                "segmentation": [seg],
-                "area": area,  # 替换为实际区域面积
-                "bbox": roi,  # 替换为实际边界框信息 [x, y, width, height]
-                "iscrowd": 0
-            }
-            ann_list.append(annotation_info)
     return image_info, ann_list
 
 
@@ -196,7 +109,7 @@ def do_coco_dataset():
     output_dir = "../output/dataset"
     # 手动下载并解压到 scene_dir
     # https://aistudio.baidu.com/datasetdetail/93975
-    scene_dir = "../output/scene_images"
+    scene_dir = "../output/scene_images_2"
     # diagram_warp_dir = "../output/diagram_warp"
     label_path = "../output/diagram_img/label.txt"
     if not os.path.exists(output_dir):
@@ -229,10 +142,10 @@ def do_coco_dataset():
         print("scene: " + scene)
         scene_path = os.path.join(scene_dir, scene)
         dia_path, warp_path = diagram_list[random.randint(0, len(diagram_list) - 1)]
-        jpg_img, offset_x, offset_y, zoom = combine_scene_image(scene_path, warp_path)
-        json_obj = offset_json_obj(warp_path + ".json", offset_x, offset_x, zoom)
+        jpg_img, offset_x, offset_y = combine_scene_image(scene_path, warp_path)
+        json_obj = offset_json_obj(warp_path + ".json", offset_x, offset_x)
 
-        img_info, ann_list = coco_image_info(json_obj, jpg_img, dia_path, warp_path, scene)
+        img_info, ann_list = coco_image_info(json_obj, jpg_img, scene)
         cv2.imwrite(os.path.join(output_dir, scene), jpg_img)
         images.append(img_info)
         annotations += ann_list
